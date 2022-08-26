04e4ff3758264547b2e58fb21de32d468a3e4f3c
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-08-26 11:14:12 -0500

    practica02 11:00

---
+++ practica02/PruebaSaludador.java
@@ -0,0 +1,13 @@
+//10:24-26/08/2022
+//Rivera Román Miguel Esaú
+//Método principal de la clase "Saludador"
+
+public class PruebaSaludador{
+	//metodo pricipal(main)
+	public static void main (String[]args){
+		//creando una instancia del Objeto saludador 
+		Saludador alumno = new Saludador();
+		//accediendo al método saludar del alumno
+		alumno.saludar();
+	}
+}
---
+++ practica02/Saludador.java
@@ -0,0 +1,12 @@
+//10:09-26/08/2022
+//Rivera Román Miguel Esaú
+//Programa que ejecute un hola mundo orientado a objetos
+
+//Definición de la clase
+
+public class Saludador{
+	//Método para saludar
+	public void saludar(){//llave que inicia el método
+		System.out.println("Hola Alumno de POO");
+	}//llave que cierra el método de saludar
+}//llave que cierra la clase
