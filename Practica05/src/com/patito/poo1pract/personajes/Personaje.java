66090a81b7e4eb3c61eab9894f48970287c9cf06
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-09 08:43:39 -0500

    Create Personaje.java

---
+++ Practica05/src/com/patito/poo1pract/personajes/Personaje.java
@@ -0,0 +1,41 @@
+//10:02-26/08/2022
+//Rivera Román Miguel Esaú
+//Cláse de la práctica 3 con constructor
+
+public class Personaje{
+	private String nombre;  
+	private int edad;
+	private boolean edad2;
+
+	public Personaje (String nombre){
+		this.nombre = nombre;
+		this.edad = 0;
+
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+
+	}
+	public String getNombre(){
+		return nombre;
+	}
+
+	public boolean setEdad(int edad){
+		if(edad>0 && edad <120){
+			edad2 = true;
+			this.edad = edad;
+			return edad2;
+		}
+		return edad2;
+	}
+
+	public int getEdad(){
+		return edad;
+	}
+	
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " + nombre);
+	}  
+
+}
