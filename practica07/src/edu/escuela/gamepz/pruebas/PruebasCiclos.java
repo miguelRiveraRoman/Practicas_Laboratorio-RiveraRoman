487dbb1ea3c7f1d19f790a478f05eae63f335b09
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-23 10:39:31 -0500

    10:39 AM

--- practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
@@ -1,9 +1,12 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.*;
+
 public class PruebasCiclos{
 	public static void main(String[] args) {
-		Personaje per01 = new Personaje("Diego", 0)
-		Personaje per01 = new Personaje("Almendra", 0)
-		Personaje per01 = new Personaje("Bianca", 0)
-		Personaje per01 = new Personaje("Jordan", 0)
+		Personaje per01 = new Personaje("Diego", 0);
+		Personaje per01 = new Personaje("Almendra", 0);
+		Personaje per01 = new Personaje("Bianca", 0);
+		Personaje per01 = new Personaje("Jordan", 0);
 	 int n,m;
 		n = 0
 		m = 0
@@ -23,6 +26,19 @@ public class PruebasCiclos{
 				System.out.println(per01.getDetalle() + " sin modificación en edad a "+ ed);
 			}
 			System.out.println(per01.getDetalle() + "se modificó la edad");
+
+			do{
+				ed =(int)(Math.random()*200);
+				System.out.println(ed);
+
+
+				if( ed > 150) {
+					System.out.println("El número generado es mayor a 150");
+					continue outfor;
+				}
+				while(per04.setEdad(ed));
+				System.out.println(per04.getDetalle());
+			}
 		}
 	}
 }
