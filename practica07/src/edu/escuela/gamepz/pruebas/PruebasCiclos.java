e5b1885d0c0976b822e0aa4bd131a91e01d5e15b
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-23 10:00:23 -0500

    10:00 AM

--- practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
@@ -11,13 +11,18 @@ public class PruebasCiclos{
 	{
 		while (n>=m); {
 			int n = (int)(Math.random()*100);
-			int m = (int)(Math.random()*100);
-			System.out.println(n,m);
-
-			
+			int m = (int)(Math.random()*100)			
 		}
-		for (; ; ) {
-			
+
+		System.out.println(n,m);
+		
+		outfor:
+		for (int i = n; i<m ; i++ ) {
+			int ed= (int)(Math.random()*250);
+			if(!per01.setEdad(ed)) {
+				System.out.println(per01.getDetalle() + " sin modificación en edad a "+ ed);
+			}
+			System.out.println(per01.getDetalle() + "se modificó la edad");
 		}
 	}
 }
