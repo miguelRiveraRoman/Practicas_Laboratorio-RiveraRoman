20e0493f56558e6a1ef5e1e9e3b718ffb2f6bb9d
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-23 08:50:24 -0500

    08:43 AM

--- practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebasCiclos.java
@@ -1,1 +1,16 @@
-public class PruebasCiclos
+public class PruebasCiclos{
+	public static void main(String[] args) {
+		Personaje per01 = new Personaje("Diego")
+		Personaje per01 = new Personaje("Almendra")
+		Personaje per01 = new Personaje("Bianca")
+		Personaje per01 = new Personaje("Jordan")
+	 int n,m;
+	this.n = 0
+	this.m = 0
+	}
+	{
+		for ( int n; n>= m ; Math.random()*100 ) {
+			
+		}
+	}
+}
