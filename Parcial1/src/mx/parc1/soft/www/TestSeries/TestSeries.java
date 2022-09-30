e973ea7fa8c9948daed360869485a97e2901e5f4
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-30 08:01:19 -0500

    Parcial1 08:01 AM

--- Parcial1/src/mx/parc1/soft/www/TestSeries/TestSeries.java
+++ Parcial1/src/mx/parc1/soft/www/TestSeries/TestSeries.java
@@ -1,13 +1,21 @@
 public class TestSeries{
 	public static void main(String[] args) {
 		Serie s = new Serie();
+		
 		int m = s.genNumero(100);
+		
 		s.primos(m);
+		
 		int o = s.primoN(m/10);
+		
 		System.out.println(o);
+		
 		int n = s.genNumero(100);
+		
 		s.fibo(n);
+		
 		int p = s.fiboN(3,11)
+		
 		System.out.println(p);
 	}
 }
