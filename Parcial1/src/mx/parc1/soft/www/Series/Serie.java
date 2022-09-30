5c63fba18a3f13cc8c17afa49b7e8359b0d7d8cb
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-09-30 09:17:14 -0500

    Parcial1 09:17 AM

--- Parcial1/src/mx/parc1/soft/www/Series/Serie.java
+++ Parcial1/src/mx/parc1/soft/www/Series/Serie.java
@@ -1,12 +1,22 @@
 public class Serie{
-	public int x = 0
-	public int y = 1
-	public int r = x + y
+	
+	public int s = (int)(Math random()*10);
+	public int x = 0;
+	public int y = 1;
+	public int r = 1;
 
 	public Serie(int x, int y, int r) {
-		this.x = x 
-		this.y = y 
-		this.r = r
+		this.x = x; 
+		this.y = y; 
+		this.r = r;
 	} 
-	
+
+   for (int i = 1; i < s; i++){
+
+   		System.out.println(r);
+
+   		r = x + y 
+   		x = y 
+   		y = r
+   }
 }
