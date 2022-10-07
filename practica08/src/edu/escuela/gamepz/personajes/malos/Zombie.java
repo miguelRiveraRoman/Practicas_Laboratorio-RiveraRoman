f3268ede842a66319e41c58e1765dc5000cdc820
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-10-07 12:00:18 -0500

    12:00 PM

--- practica08/src/edu/escuela/gamepz/personajes/malos/Zombie.java
+++ practica08/src/edu/escuela/gamepz/personajes/malos/Zombie.java
@@ -1,7 +1,64 @@
 public class Zombie extends Personaje{
 	public boolean ataque;
 
-	public boolean getAtaque(booleab ataque){
+	public Zombie(String nombre, int vida, boolean ataque){
+		super(nombre,vida);
+		this.ataque = ataque 
+	}
+	public Zombie(String nombre, boolean ataque){
+		super(nombre);
+	}
+	public Zombie(String nombre){
+		super(nombre);
+	}
+
+	public String getDetalle(){
+		super.getDetalle();		
+	}
+
+	public boolean getAtaque(boolean ataque){
 		return ataque;
+	
+	public int decVida(boolean ataque){
+		super.decVida(){
+			if (ataque = false) {
+				this.vida = vida*3
+			}		
+			else (ataque = true) {
+				this.vida = vida*2
+			}
+		}
+	}
+	public int decVida(boolean ataque){
+		super.decVida(){ 
+			if (ataque = false) {
+				this.vida = vida*3
+			}		
+			else (ataque = true) {
+				this.vida = vida*2
+			}
+		}
+	}
+	public int addVida(boolean ataque){
+		super.addVida(){
+			if (ataque = true) {
+				this.vida = vida*3
+			}				
+			else (ataque = false){
+				this.vida = vida
+			}
+		}
+	}
+	public int addVida(boolean ataque){
+		super.addVida(){
+			if (ataque = true) {
+				this.vida = vida*3
+			}				
+			else (ataque = false){
+				this.vida = vida
+			}				
+		}
+	}	
+
 	}
 }
