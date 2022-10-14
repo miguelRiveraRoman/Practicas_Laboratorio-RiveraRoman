4b783351329088396e7c77d039e98d74e4363723
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-10-14 08:28:54 -0500

    08:28 AM

--- practica09/Casas.java
+++ practica09/Casas.java
@@ -1,1 +1,31 @@
-Casas
+public class Casa{
+	private String tipo;
+	private int habitaciones;
+	private float costo;
+
+	public Casa(String tipo, int habitaciones, float costo){
+		this.tipo = tipo;
+		this.habitaciones = habitaciones;
+		this.costo = costo;
+	}
+
+	public Casa(String tipo){
+		habitaciones = 2;
+		costo = 2000000;
+	}
+
+	public int hasCode(){
+		return nombre.hasCode()
+	}
+
+	public boolean equals(Object o){
+		if (o != null && o instanceof Perrito ) {
+			Perrito p = (Perrito) o;
+
+			if (nombre.equals(p.nombre) && (edad == p.edad) ) {
+		  		return true;
+		  }  
+		}
+		return false;
+	}
+}
--- practica09/campo.java
+++ practica09/campo.java
@@ -1,4 +1,12 @@
 public class Campo extends Casas{
-	private float piscina[];
 
+	private float piscina[0,1];
+
+	public Campo(){
+
+	}
+
+	public Campo(){
+		
+	}
 }
