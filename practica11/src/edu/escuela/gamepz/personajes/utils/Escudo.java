eb00364843f66de4e2a8b2f8febc25075bd9cbe4
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-10-28 11:11:52 -0500

    Práctica11 11:11 AM

--- practica11/src/edu/escuela/gamepz/personajes/utils/Escudo.java
+++ practica11/src/edu/escuela/gamepz/personajes/utils/Escudo.java
@@ -2,9 +2,16 @@ public enum Escudo{
 
 	SUPER(0),
 	MEDIO(1), 
-	BAJO(2)
-	NULO(3)
+	BAJO(2),
+	NULO(3);
 
 	private final int nivel;
 
+	public Escudo(int nivel){
+		this.nivel = nivel;
+	}
+
+	public String getNivel(){
+		return nivel;
+	}
 }
