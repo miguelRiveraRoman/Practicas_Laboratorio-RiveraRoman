674b769761c7a829e7ef40ae937e5c673ffd89a7
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-11-04 11:31:39 -0600

    practica12 11:31 AM

---
+++ practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -0,0 +1,22 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.*;
+import edu.escuela.gamepz.personajes.malos.*;
+import edu.escuela.gamepz.personajes.buenos.*; 
+import edu.escuela.gamepz.utils.*;
+
+
+public class PruebaTablero{
+	public static void main(String[] args) {
+		Personaje[] datos = {
+			new Planta ("David",100),
+			new Zombie ("Bianca"),
+			new Planta ("Fabian",10,Escudo.MEDIO),
+			new Planta ("Almendra",50),
+			new Planta ("Ricardo",Escudo.BAJO),
+			new Planta ("Silvia"),
+			new Zombie ("Armando",80,false),
+			new Zombie ("Josseline",true),
+			new Zombie ("Eduardo")
+		};
+	}
+}
