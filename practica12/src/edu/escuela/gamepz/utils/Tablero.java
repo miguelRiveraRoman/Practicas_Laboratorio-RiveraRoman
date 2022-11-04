b5b7736e54564b7b813b72e013d52cbd208c94fd
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-11-04 09:28:33 -0600

    practica12 9:28 AM

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -6,8 +6,9 @@ public class Tablero{
 	public static final int MAX_SIZE = 10;
 	public static final Personaje[] personajes = new Personaje[MAX_SIZE];
 
-	private Tablero(int MAX_SIZE){
+	private Tablero(int MAX_SIZE){}
 
-
-	}
+	public static void mostrarT(){
+		System.out.println("\nInicio ***Contenido del areeglo en Tablero");
+	} 
 }
