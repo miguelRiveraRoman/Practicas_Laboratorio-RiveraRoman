1edff831ca1b9437b117707195cb00a1aa5f47eb
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-11-11 11:44:16 -0600

    practica13 11:44 AM

--- practica13/src/edu/escuela/gamepz/utils/ByVida.java
+++ practica13/src/edu/escuela/gamepz/utils/ByVida.java
@@ -7,12 +7,13 @@ public class ByVida implements Comparator<Personaje>{
 		if (p1.getVida() != p2.getVida()){
 			return (p2.getVida() < p1.getVida()) ? -1 : 1;
 		}
-		if (p2.getVida() == p1.getVida()){
-			return (p1.getNombre().compareTo(p2.getNombre());
-		}
-		if (p1.getVida() == p2.getVida() && p1.getNombre() == p2.getNombre() ) {
+		
+		return (p1.getNombre().compareTo(p2.getNombre());
+		
+		if (p1.getNombre() == p2.getNombre() ) {
 			return (p2.getVida() > p1.getVida()) ? -1 : 1;
 			
 		}
+
 	}
 }
