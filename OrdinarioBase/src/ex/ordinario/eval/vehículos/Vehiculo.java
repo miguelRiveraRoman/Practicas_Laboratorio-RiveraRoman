ca9930d120572b49cd488b7ea39b0b10011a47c7
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-12-06 10:13:32 -0600

    10:13 AM Ordinario

--- OrdinarioBase/src/ex/ordinario/eval/vehículos/Vehiculo.java
+++ OrdinarioBase/src/ex/ordinario/eval/vehículos/Vehiculo.java
@@ -1,6 +1,26 @@
 public class Vehiculo {
-	String marca;
-	int modelo;
-	float costo;
+	private String marca;
+	private int modelo;
+	private float costo;
 
+	public Vehiculo(String marca, int modelo, float costo){
+		this.marca = marca;
+		this.modelos = modelo;
+		this.costo = costo;
+	}
+
+	public String getMarca(){
+		return marca;
+	}
+	public int getModelos(){
+		return modelo;
+	}
+	public float getCosto(){
+		return costo;
+	}
+
+
+	public String toString(){
+		return marca +"\t"+modelo+"\t "+costo"\t";
+	}
 }
