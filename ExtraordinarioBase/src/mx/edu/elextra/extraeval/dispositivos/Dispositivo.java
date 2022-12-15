1508e525473e2663e9aafa11b8434e2a6fad040d
miguelRiveraRoman <ultralitogodofredo@gmail.com>
2022-12-15 09:14:47 -0600

    Extraordinario 09:14 AM

--- ExtraordinarioBase/src/mx/edu/elextra/extraeval/dispositivos/Dispositivo.java
+++ ExtraordinarioBase/src/mx/edu/elextra/extraeval/dispositivos/Dispositivo.java
@@ -1,14 +1,32 @@
-public class Dispositivo {
+package mx.edu.elextra.extraeval.dispositivos;
+
+public class Dispositivo implements Comparable<Dispositivo> {
 
 	private String marca;
 	private float costo;
 
-	public Dispositivo(String marca float costo){
+	public Dispositivo(String marca, float costo){
 		this.marca = marca;
 		this.costo = costo;
 	}
 
+	public String getMarca(){
+		return marca;
+	}
+	public float getCosto(){
+		return costo;
+	}
+
 	public String toString(){
 		return marca + "\t"+ costo;
+	}
+
+	public int compareTo(Dispositivo o){
+		if(this.marca.compareTo(o.marca) !=0){
+			return this.marca.compareTo(o.marca);
+		}
+		if(this.costo != o.costo){
+			return o.costo -  ((int) this.costo);
+		}		
 	}
 }
